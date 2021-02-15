e72cc3c952f8ef424028d32d5e608802fb119d1f
Rajesh JUYALl <juyalrajesh36@gmail.com>
2021-02-15 23:51:59 +0530

    working on class module

--- src/app/class/class.component.ts
+++ src/app/class/class.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../crud.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-class',
   templateUrl: './class.component.html',
-  styleUrls: ['./class.component.scss']
+  styleUrls: ['./class.component.scss'],
 })
 export class ClassComponent implements OnInit {
+  constructor(private api: CrudService, private http: HttpClient) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
